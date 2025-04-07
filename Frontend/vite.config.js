fa90853d8b10869f0f623db3178e52826a9fdbb7
Prantik Sanki <prantiksanki@gmail.com>
2025-04-07 23:56:58 +0530

    UI add

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5000,
+    open: true,
+    // allowedHosts:all,
+
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
