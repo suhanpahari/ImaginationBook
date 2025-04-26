e7494ff10d41aaecc3db9b8d78ef75a7aed58323
Prantik Sanki <prantiksanki@gmail.com>
2025-04-26 21:56:50 +0530

    port

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const bcrypt = require('bcrypt');
 dotenv.config();
 
 const app = express() ; 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 // const ngrok = process.env.NGROK_ENDPOINT ;
 
 const allowedOrigins = [
